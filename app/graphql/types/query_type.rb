469091476fb78b27d4e22aa1064aac1c8a4a2137
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-08-29 13:21:08 -0400

    Create get_users and get_users_by_id_endpoint
    Co-authored-by: Nick Jacobs <nickjacobsatc@gmail.com>
    Co-authored-by: Caden Jarrett <caden.jarrett99@gmail.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,19 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :get_users, [Types::UserType], null: false 
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def get_users
+      User.all
     end
+
+    field :get_user_by_id, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def get_user_by_id(id:)
+      User.find(id)
+    end
+
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :name, String
     field :email, String
+    field :items, [Types::ItemType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end

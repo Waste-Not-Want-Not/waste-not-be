c8be01b4d3f07b5bc3878dd5c5ea9ff687adfbff
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-30 15:24:11 -0500

    Migration: added donation column to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_142315) do
+ActiveRecord::Schema.define(version: 2022_08_30_201805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_08_29_142315) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "for_donation", default: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 

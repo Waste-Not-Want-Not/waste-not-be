05867cdb8c09109df9507747102e8fa053d99234
Nick Jacobs <nickjacobsatc@gmail.com>
2022-09-01 18:23:48 -0500

    Feat: getFoodBank query now usable, and tested

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,7 +4,7 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    field :get_users, [Types::UserType], null: false 
+    field :get_users, [Types::UserType], null: false
 
     def get_users
       User.all
@@ -16,6 +16,21 @@ module Types
 
     def get_user_by_id(id:)
       User.find(id)
+    end
+
+    field :get_food_bank, Types::FoodBankType, null: false do
+      argument :location, String, required: true
+    end
+
+    def get_food_bank(location:)
+      info = FoodBankFacade.get_info(location)
+      directions = RouteFacade.get_route(location, "#{info.address}")
+      output = {
+        name: info.name,
+        address: info.address,
+        phone_number: info.phone_number,
+        directions: directions.directions_array.join(", ")
+      }
     end
 
   end

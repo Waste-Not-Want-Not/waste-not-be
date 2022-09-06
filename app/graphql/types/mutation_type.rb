6b4418de5ce579b53c3e2dd477a66440451425cc
Caden Jarrett <caden.jarrett99@gmail.com>
2022-09-06 09:46:15 -0600

    Added update user item to our mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,16 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
-<<<<<<< HEAD
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
-
     field :create_user_item, mutation: Mutations::CreateUserItem
-=======
     field :create_item, mutation: Mutations::CreateItem
->>>>>>> e07a710d9a7900d0b5490109c26829fbbb256383
+    field :update_item, mutation: Mutations::UpdateItem
   end
 end

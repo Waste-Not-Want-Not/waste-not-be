82069cca0f37f78d9a3dc93a296cb5f99ff4b34a
Caden Jarrett <caden.jarrett99@gmail.com>
2022-09-06 11:20:20 -0600

    refactored mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user_item, mutation: Mutations::CreateUserItem
     field :create_item, mutation: Mutations::CreateItem
-    field :update_item, mutation: Mutations::UpdateItem
+    field :update_for_donation, mutation: Mutations::UpdateForDonation
   end
 end

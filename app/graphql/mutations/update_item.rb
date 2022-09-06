cd87126a38daa4ba3ba8ed0c90f1efb0cf3f1b3a
Caden Jarrett <caden.jarrett99@gmail.com>
2022-09-06 09:46:44 -0600

    added update item graphql mutation

--- app/graphql/mutations/update_item.rb
+++ app/graphql/mutations/update_item.rb
@@ -1,18 +1,17 @@
 class Mutations::UpdateItem < Mutations::BaseMutation
-  argument :user_id, Integer, required: false
   argument :name, String, required: false
   argument :location, String, required: false
   argument :expiration_date, String, required: false
-  # argument :image, String, required: false
-  argument :for_donation, Boolean, required: false
+  argument :for_donation, String, required: false
+  argument :id, Integer, required: true
 
   field :item, Types::ItemType, null: false 
   field :errors, [String], null: false 
 
-  def resolve(name:, user_id:, location:, expiration_date:, for_donation:)
-    item = Item.update(name: name, user_id: user_id, location: location, expiration_date: expiration_date, image: "fakeimage")
-    binding.pry
-    if item.save 
+  def resolve(name:, location:, expiration_date:, for_donation:, id:)
+    item = Item.find(id)
+    update = item.update!(name: name, location: location, expiration_date: expiration_date, for_donation: for_donation)
+    if update == true 
       {
         item: item,
         errors: []

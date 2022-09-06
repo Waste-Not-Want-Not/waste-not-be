75760018a48622ad0dc97b06739e266659fe8ed2
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-09-06 13:03:01 -0400

    Add production link to cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' #, 'https://waste-not-fe.herokuapp.com'
+    origins 'http://localhost:3000', 'https://waste-not-wn.herokuapp.com'
 
     resource '*',
       headers: :any,

1ebae9242934b8e13be53f9f2767a4206897d7bf
Jennifer Halloran <48455658+jenniferhalloran@users.noreply.github.com>
2022-11-21 14:35:31 -0500

    Update cors for new deployment on vercel

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://waste-not-wn.herokuapp.com'
+    origins 'http://localhost:3000', 'https://waste-not-wn.herokuapp.com', 'https://waste-not-delta.vercel.app/'
 
     resource '*',
       headers: :any,
